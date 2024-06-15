878ca223501d6e69d79090c8c26f14629963b4b9
Mohith Gowda K <mohithgowdak.kote@gmail.com>
2024-06-15 12:46:27 +0530

    21

--- Analysis_of_algorithms/sorting_algorithms/counting_sort.py
+++ Analysis_of_algorithms/sorting_algorithms/counting_sort.py
@@ -1,4 +1,12 @@
 def count_sort(input_array):
+	'''The time complexity of the count_sort function is O(n + k), where n is the number of elements in the input
+	  array and k is the range of the input values. This is because the function iterates through the input array twice - once to count the frequency of each element and 
+	  once to create the sorted output array. 
+	  The sorting step takes O(n) time, while the counting step takes O(k) time.
+
+      The space complexity of the count_sort function is O(n + k), where n is the number of elements in the input array and k is the range of the input values. 
+      This is because the function uses a dictionary to store the frequency of each element, 
+      which can have up to k unique keys, and an output array that can have up to n elements'''
 	'''
 	maxi=max(input_array)
 	count={}
@@ -16,10 +24,23 @@ def count_sort(input_array):
 	return output
 	# this code is not stable. because it does not check for the position of same elements
 	'''
-	
-	
+	maxi = max(input_array)
+	count=[0]*(maxi+1)
+	# finding individual frequency
+	for x in input_array:
+		count[x] +=1
+	# finding cumulative frequency
+	for i in range(1,len(count)):
+		count[i] += count[i-1]
+	# adding the array elements to output array
+	output=[0]*len(input_array)
+	for i in range(len(input_array)-1,-1,-1):
+		# fetching the index value from cumulative frequency . -1 is done.
+		output[count[input_array[i]]-1]=input_array[i]
+		# reducing the frequency
+		count[input_array[i]]-=1
+	return output	
 
-# Driver code
 if __name__ == "__main__":
 	
 	# Input array
